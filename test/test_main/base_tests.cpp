ae2a8a42b527865c059f47008bff6bd0bfaf30c0
Egor Dmitriev <egordmitriev2@gmail.com>
2018-02-07 09:04:26 +0100

    Initial commit

---
+++ test/test_main/base_tests.cpp
@@ -0,0 +1,20 @@
+//
+// Created by egordm on 5-2-18.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class BaseTests : public ::testing::Test {
+
+};
+
+TEST_F(BaseTests, BaseTests_HelloWorld_Test) {
+    std::cout << "Hello, World!" << std::endl;
+    ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
