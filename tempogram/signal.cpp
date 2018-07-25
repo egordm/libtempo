5c59786d118dedcf97ee99a94bbb2b087035de9e
Egor Dmitriev <egordmitriev2@gmail.com>
2018-07-25 16:29:22 +0200

    Setup needed libraries

--- tempogram/signal.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by egordm on 25-7-2018.
-//
-
-#include "signal.h"
--- tempogram/tempogram.h
+++ tempogram/tempogram.h
@@ -5,13 +5,8 @@
 #ifndef TEMPOGRAM_TEMPO_ESTIMATION_COMPUTE_FOURIER_COEFFICIENTS_H
 #define TEMPOGRAM_TEMPO_ESTIMATION_COMPUTE_FOURIER_COEFFICIENTS_H
 
-#include <Eigen/Dense>
-
-using namespace Eigen;
-
 namespace tempogram {
-    ArrayXXd compute_fourier_coefficients(const ArrayXd &s, const ArrayXd &window, int n_overlap, const ArrayXd &f,
-                                      float sr = 1);
+
 
 }
 
