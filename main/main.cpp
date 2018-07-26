22e93d04c7beb97ed91f649c9aee874e3e3ffc17
Egor Dmitriev <egordmitriev2@gmail.com>
2018-07-26 18:52:49 +0200

    Fixed bugs on tempogram calculation

--- main/main.cpp
+++ main/main.cpp
@@ -4,9 +4,12 @@
 
 int main() {
 
-    arma::vec A = linspace<vec>(0, 9, 10);
-    std::cout << "A:\n" << A(span(2,5)) << "\n";
-    std::cout << "AA:\n" << A << "\n";
+    arma::vec A(10);
+    A.ones();
+    arma::vec B = linspace<vec>(0, 9, 10);
+
+    std::cout << tempogram::novelty_curve_to_tempogram_dft(A, B, 10, 10, 10) << std::endl;
+
 
     return 0;
 }
--- tempogram/compute_fourier_coefficients.cpp
+++ tempogram/compute_fourier_coefficients.cpp
@@ -8,32 +8,31 @@
 #include <cmath>
 
 using namespace tempogram::utils;
+using namespace arma;
 
-arma::mat
-tempogram::compute_fourier_coefficients(const arma::vec &s, const arma::vec &window, int n_overlap, const arma::vec &f,
-                                        double sr) {
+mat  tempogram::compute_fourier_coefficients(const vec &s, const vec &window, int n_overlap, const vec &f, double sr) {
     int win_length = (int)window.size();
     int hop_length = win_length - n_overlap;
 
-    auto T = arma::linspace<arma::vec>(0, win_length - 1, static_cast<const arma::uword>(win_length)) / sr;
+    vec T = linspace<vec>(0, win_length - 1, static_cast<const uword>(win_length)) / sr;
     int win_num = math::fix((s.size() - n_overlap) / (win_length - n_overlap));
-    arma::mat x(static_cast<const arma::uword>(win_num), f.size());
+    mat x(static_cast<const uword>(win_num), f.size());
 
-    auto twoPiT = 2 * M_PI * T;
+    vec twoPiT = 2 * M_PI * T;
 
     for(int f0 = 0; f0 < f.size(); ++f0) {
-        auto twoPiFt = f[f0] * twoPiT;
-        auto cosine = cos(twoPiFt);
-        auto sine = sin(twoPiFt);
+        vec twoPiFt = f[f0] * twoPiT;
+        vec cosine = cos(twoPiFt);
+        vec sine = sin(twoPiFt);
 
         for(int w = 0; w < win_num; ++w) {
             int start = w * hop_length;
             int stop = start + win_length;
 
-            auto sig = s.subvec((const arma::uword)start, (const arma::uword)(stop - 1)) * window;
-            auto co = sum(sig * cosine);
-            auto si = sum(sig * sine);
-            x[w, f0] = (co + 1 * si);
+            vec sig = s.subvec((const uword)start, (const uword)(stop - 1)) % window;
+            auto co = sum(sig % cosine);
+            auto si = sum(sig % sine);
+            x((const uword)w, (const uword)f0) = (co + 1 * si);
         }
     }
 
