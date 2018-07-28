f313b5293aef62ee425dee899d243a5da6e13234
Egor Dmitriev <egordmitriev2@gmail.com>
2018-07-29 00:45:13 +0200

    Added stft

--- tempogram/compute_fourier_coefficients.cpp
+++ tempogram/compute_fourier_coefficients.cpp
@@ -4,12 +4,14 @@
 
 #define _USE_MATH_DEFINES
 
+#include <sigpack.h>
 #include "compute_fourier_coefficients.h"
 #include "math_utils.hpp"
-#include <cmath>
 
 using namespace tempogram;
 using namespace arma;
+using namespace sp;
+
 
 std::tuple<cx_mat, vec, vec>
 tempogram::compute_fourier_coefficients(const vec &s, const vec &window, int n_overlap, const vec &f, double sr) {
@@ -20,7 +22,7 @@ tempogram::compute_fourier_coefficients(const vec &s, const vec &window, int n_o
     vec T = linspace<vec>(0, win_length - 1, static_cast<const uword>(win_length)) / sr;
     int win_num = utils::math::fix((s.size() - n_overlap) / (win_length - n_overlap));
     cx_mat x(static_cast<const uword>(win_num), f.size(), fill::zeros);
-    vec t = linspace(win_length_half, win_length_half + (win_num - 1) * hop_length, (const uword)win_num) / sr;
+    vec t = linspace(win_length_half, win_length_half + (win_num - 1) * hop_length, (const uword) win_num) / sr;
 
     vec twoPiT = 2 * M_PI * T;
 
@@ -54,12 +56,119 @@ cx_mat tempogram::normalize_feature(const cx_mat &feature, unsigned int p, doubl
     unit_vec.ones();
     unit_vec = unit_vec / norm(unit_vec, p);
 
-    for(int k = 0; k < feature.n_cols; ++k) {
-        double n = norm(feature(span::all, (const uword)k), p);
+    for (int k = 0; k < feature.n_cols; ++k) {
+        double n = norm(feature(span::all, (const uword) k), p);
 
-        if(n < threshold) ret(span::all, (const uword)k) = unit_vec;
-        else ret(span::all, (const uword)k) = feature(span::all, (const uword)k) / n;
+        if (n < threshold) ret(span::all, (const uword) k) = unit_vec;
+        else ret(span::all, (const uword) k) = feature(span::all, (const uword) k) / n;
     }
 
     return ret;
+}
+
+tuple<mat, float, vec, vec> tempogram::stft(const vec &s, int sr, const vec &window, int n_fft, int hop_length) {
+    std::tuple<int, int> coefficient_range = make_tuple(0, (int) floor(max(n_fft, (int) window.size()) / 2.) + 1);
+    return stft(s, sr, window, coefficient_range, n_fft, hop_length);
+}
+
+tuple<mat, float, vec, vec>
+tempogram::stft(const vec &s, int sr, const vec &window, std::tuple<int, int> coefficient_range, int n_fft,
+                int hop_length) {
+    auto window_length = static_cast<int>(window.size());
+    if (hop_length <= 0) hop_length = static_cast<int>(round(window_length / 2.));
+    if (n_fft <= 0) n_fft = window_length;
+
+    // Precalculate
+    float feature_rate = (float) sr / hop_length;
+    auto signal_size = static_cast<int>(s.size());
+
+    auto first_window = (int) floor(window_length / 2.);
+    auto num_frames = (int) ceil((double) signal_size / hop_length);
+    int num_coeffs = std::get<1>(coefficient_range) - std::get<0>(coefficient_range);
+    int n_zero_pad = max(0, n_fft - window_length);
+
+    // Spectrogram calculation
+    mat spec((const uword) num_coeffs, (const uword) num_frames, fill::zeros);
+
+    // first window's center is at 0 seconds
+    ivec frame = linspace<ivec>(0, window_length - 1, (const uword) window_length) - first_window;
+    for (int i = 0; i < num_frames; ++i) {
+        int n_zeros = static_cast<int>(sum(frame <= 0));
+        int n_vals = static_cast<int>(frame.size() - n_zeros);
+
+        vec x(frame.size());
+        if (n_zeros > 0)
+            x = join_cols(vec((const uword) n_zeros, fill::zeros), s(span(0, (const uword) n_vals)));
+        else if (frame(frame.size() - 1) >= signal_size)
+            x = join_cols(s(span((const uword) frame(0), s.size())),
+                          vec(window_length - (s.size() - frame(0)), fill::zeros));
+        else x = s(span((const uword) frame(0), (const uword) frame(frame.size() - 1)));
+
+        x %= window;
+
+        if (n_zero_pad > 0) x = join_cols(x, vec((const uword) n_zero_pad, fill::zeros));
+
+        cx_vec Xs = fft(x);
+        Xs = Xs(span((const uword) std::get<0>(coefficient_range), (const uword) std::get<1>(coefficient_range)));
+        spec(span::all, (const uword) i) = abs(Xs); // Convert to magnitude. Not complex anymore
+
+        frame += hop_length;
+    }
+
+    vec t = linspace<vec>(0, spec.n_cols - 1, spec.n_cols) * hop_length / (double) sr;
+    auto zet = (int) floor(max(n_fft, window_length) / 2.);
+    vec f = linspace<vec>(0, zet - 1, (const uword) zet) / (double) zet * (sr / 2.);
+    f = f(span((const uword) std::get<0>(coefficient_range), (const uword) std::get<1>(coefficient_range)));
+
+    return make_tuple(spec, feature_rate, t, f);
+}
+
+vec tempogram::audio_to_novelty_curve(const vec &s, int sr, int window_length, int hop_length, double compression_c,
+                                      bool log_compression, int resample_feature_rate) {
+    if (window_length <= 0) window_length = static_cast<int>(1024 * sr / 22050.);
+    if (hop_length <= 0) hop_length = static_cast<int>(512 * sr / 22050.);
+
+    vec window = sp::hanning(static_cast<const uword>(window_length));
+    auto stft_ret = stft(s, sr, window, window_length, hop_length);
+
+    // normalize and convert to dB
+    mat spe = std::get<0>(stft_ret) / max(std::get<0>(stft_ret));
+    double thresh = -74;
+    thresh = pow(10, thresh / 20);
+    spe = clamp(spe, thresh, spe.max());
+
+    // bandwise processing
+    mat bands = {{0,      500},
+                 {500,    1250},
+                 {1250,   3125},
+                 {3125,   7812.5},
+                 {7812.5, floor(sr / 2.)}};
+    mat band_novelty_curve(bands.n_rows, spe.n_cols, fill::zeros);
+
+    for (int i = 0; i < bands.n_rows; ++i) {
+        vec band = bands(static_cast<const uword>(i), span::all);
+        vec bins = round(band / (sr / (double) window_length));
+        bins = clamp(bins, 0, window_length / 2.);
+
+        // band novelty curve
+        mat band_data = spe(span((const uword) bins(0), (const uword) bins(1)), span::all);
+        if (log_compression && compression_c > 0)
+            band_data = log(1 + band_data * compression_c) / log(1 + compression_c);
+
+        // smoothed differentiator
+        double diff_len = 0.3;
+        diff_len = max(ceil(diff_len * sr / (double) hop_length), 5.);
+        diff_len = 2 * round(diff_len / 2.) + 1;
+
+        vec mult_filt = join_cols(-1 * vec((const uword) floor(diff_len / 2), fill::ones), vec(1, fill::zeros));
+        mult_filt = join_cols(mult_filt, vec((const uword) floor(diff_len / 2), fill::ones));
+        vec diff_filter_vec = sp::hanning((const uword) diff_len) % mult_filt;
+        mat diff_filter(1, diff_filter_vec.size());
+        diff_filter(0, span::all) = diff_filter_vec;
+
+        //mat band_krn = join_cols()
+
+    }
+
+    return arma::vec();
 }
--- tempogram/compute_fourier_coefficients.h
+++ tempogram/compute_fourier_coefficients.h
@@ -6,6 +6,8 @@
 #define PROJECT_COMPUTE_FOURIER_COEFFICIENTS_H
 
 #include <armadillo>
+#include <cmath>
+
 
 using namespace arma;
 
@@ -18,7 +20,8 @@ namespace tempogram {
      * @param sr sampling rate of signal s in Hz
      * @return
      */
-    std::tuple<cx_mat, vec, vec> compute_fourier_coefficients(const arma::vec &s, const arma::vec &window, int n_overlap, const arma::vec &f, double sr);
+    std::tuple<cx_mat, vec, vec> compute_fourier_coefficients(const vec &s, const vec &window, int n_overlap,
+                                                              const vec &f, double sr);
 
     /**
      * Normalizes a feature sequence according to the l^p norm
@@ -30,6 +33,16 @@ namespace tempogram {
      * @return
      */
     cx_mat normalize_feature(const cx_mat &feature, unsigned int p, double threshold);
+
+    std::tuple<mat, float, vec, vec>
+    stft(const vec &s, int sr, const vec &window, std::tuple<int, int> coefficient_range, int n_fft = -1, int hop_length = -1);
+
+    std::tuple<mat, float, vec, vec>
+    stft(const vec &s, int sr, const vec &window, int n_fft = -1, int hop_length = -1);
+
+    vec audio_to_novelty_curve(const vec &s, int sr, int window_length = -1, int hop_length = -1,
+                                      double compression_c = 1000, bool log_compression = true,
+                                         int resample_feature_rate = 200);
 }
 
 
