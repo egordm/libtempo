5730fadd30a27bee063dcbb4e6e5e11698d9b618
Egor Dmitriev <egordmitriev2@gmail.com>
2018-09-05 15:28:47 +0200

    Fixed osu formatting

--- main/present_utils.cpp
+++ main/present_utils.cpp
@@ -21,7 +21,7 @@ std::string present_utils::section_to_osu(const curve_utils::Section &section) {
     std::stringstream ss;
     ss << (uword) (section.offset * 1000) << ","
        << (uword) (60000 / section.bpm) << ","
-       << "4,1,0,100,0,1";
+       << "4,1,0,45,1,0";
 
     return ss.str();
 }
