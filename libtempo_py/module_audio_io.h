b5b19f5428301980ad235245214d5cad2876ffed
Egor Dmitriev <egordmitriev2@gmail.com>
2018-09-21 13:23:54 +0200

    Added python bindings for audio read/write

--- libtempo_py/module_audio_io.h
+++ libtempo_py/module_audio_io.h
@@ -6,8 +6,26 @@
 #define PROJECT_MODULE_AUDIO_IO_H
 
 #include <pybind11/pybind11.h>
+#include "wrapper_audio_io.hpp"
+
 
 void register_audio_io(pybind11::module &m) {
+    py::class_<wrapper_audio_io::AudioWrapper>(m, "AudioWrapper")
+            .def(py::init<const char *>())
+            .def("get_path", &wrapper_audio_io::AudioWrapper::get_path,
+                 R"pbdoc(Gets path of the audio file)pbdoc")
+            .def("get_sr", &wrapper_audio_io::AudioWrapper::get_sr,
+                 R"pbdoc(Gets audio sample rate)pbdoc")
+            .def("get_format", &wrapper_audio_io::AudioWrapper::get_format,
+                 R"pbdoc(Gets audio format as a bitmask)pbdoc")
+            .def("get_data", &wrapper_audio_io::AudioWrapper::get_data,
+                 R"pbdoc(Gets audio data as a matrix wrapper)pbdoc")
+            .def("set_data", &wrapper_audio_io::AudioWrapper::set_data,
+                 R"pbdoc(Sets audio data with a matrix wrapper)pbdoc",
+                 py::arg("data"))
+            .def("save", &wrapper_audio_io::AudioWrapper::save,
+                 R"pbdoc(Saves audio to a given path)pbdoc",
+                 py::arg("path"));
 
 }
 
--- libtempo_py/wrapper_audio_io.hpp
+++ libtempo_py/wrapper_audio_io.hpp
@@ -6,10 +6,37 @@
 #define PROJECT_AUDIO_IO_WRAPPER_HPP
 
 #include "wrapper_libtempo.hpp"
+#include <audio_processing.h>
 
+using namespace libtempo;
 using namespace wrapper_libtempo;
 
 namespace wrapper_audio_io {
+    class AudioWrapper {
+    private:
+        audio::AudioFile audio;
+
+    public:
+        explicit AudioWrapper(const char *path) : audio(audio::AudioFile::open(path)) {}
+
+        std::string get_path() { return audio.path; }
+
+        int get_sr() { return audio.sr; }
+
+        int get_format() { return audio.format; }
+
+        MatrixWrapper<float> get_data() { return MatrixWrapper<float>(audio.data);}
+
+        void set_data(const MatrixWrapper<float> &data) { audio.data = data.data;}
+
+        void save(const char *path) {
+            try {
+                audio.save(path);
+            } catch (const std::runtime_error &error) {
+                std::cerr << error.what() << std::endl;
+            }
+        }
+    };
 
 }
 
